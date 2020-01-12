c3aec6e94227680cbd1c55c00dea243756b68702
Dante12129 <dan9coasterdan@gmail.com>
2020-01-12 04:45:50 -0500

    Show Random Pokemon On Page Access

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
-    <Pokemon name="Bulbasaur" id="1" />
+    <Pokemon :name="name" :id="id" />
   </div>
 </template>
 
 <script>
 import Pokemon from "@/components/Pokemon";
+import Utils from "@/js/Utils.js";
 
 export default {
   name: 'app',
   components: {
     Pokemon
+  },
+  data: function() {
+    return {
+      name: 'None',
+      id: 0
+    };
+  },
+  created: function() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${Utils.randomInt(1, 807)}/`).then(response => response.json()).then(json => {
+      console.log(json);
+      this.name = json['name'];
+      this.id = json['id'];
+    });
   }
 }
 </script>
